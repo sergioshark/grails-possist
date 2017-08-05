6ba55a1b666b341ea866ac2f4049aa74cbd51efd
Romulo Fagundes Cantanhede <romulo.fagundes@webonesystem.com.br>
2017-08-05 15:03:39 -0300

    - Carga inicial de dados - BD. - Internacionalização.

--- grails-app/init/possist/BootStrap.groovy
+++ grails-app/init/possist/BootStrap.groovy
@@ -1,8 +1,18 @@
 package possist
 
+import br.edu.unirn.TipoTarefa
+import br.edu.unirn.tipos.StatusTarefa
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Environment.developmentMode) {
+            ["Desenvolvimento", "Suporte", "Testes", "Concluído"].each { tipo ->
+                TipoTarefa.findByDescricao(tipo) ?: new TipoTarefa(descricao: tipo).save(flush: true)
+            }
+
+        }
     }
     def destroy = {
     }
