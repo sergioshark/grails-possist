0d78321e2218cb3178dfd4c690a685895b7ca9f4
matheusmarques <matheuslucena@outlook.com>
2017-08-19 16:26:16 -0300

    Autenticacao do sistema

--- grails-app/init/possist/BootStrap.groovy
+++ grails-app/init/possist/BootStrap.groovy
@@ -1,6 +1,7 @@
 package possist
 
 import br.edu.unirn.TipoTarefa
+import br.edu.unirn.Usuario
 import br.edu.unirn.tipos.StatusTarefa
 import grails.util.Environment
 
@@ -10,6 +11,13 @@ class BootStrap {
         if (Environment.developmentMode) {
             ["Desenvolvimento", "Suporte", "Testes", "Concluído"].each { tipo ->
                 TipoTarefa.findByDescricao(tipo) ?: new TipoTarefa(descricao: tipo).save(flush: true)
+            }
+
+            if(!Usuario.findByEmail("admin@admin.com")){
+                Usuario usuario = new Usuario()
+                usuario.email = "admin@admin.com"
+                usuario.senha = "admin"
+                usuario.save(flush: true)
             }
 
         }
